90a38ad1f2ab8a294e57f44ead2f2b7d1b6a5f93
nebozoid <nebojsa.kozlovacki@gmail.com>
2021-06-17 22:52:05 +0200

    Catch restoring state exceptions

--- src/services/state/restore-service.js
+++ src/services/state/restore-service.js
@@ -6,12 +6,17 @@ const OrderService = require("../order/registration-service");
 
 module.exports = {
   restorePreviousState: async () => {
-    const data = await Order.find({ status: "pending" });
+    try {
+      const data = await Order.find({ status: "pending" });
 
-    console.log("Got all the pending orders from db");
-    for (let pendingOrder of data) {
-      pendingOrder = await OrderService.addCookingTimeToOrder(pendingOrder);
-      await OrderService.addOrder(pendingOrder);
+      console.log("Got all the pending orders from db");
+      for (let pendingOrder of data) {
+        pendingOrder = await OrderService.addCookingTimeToOrder(pendingOrder);
+        await OrderService.addOrder(pendingOrder);
+      }
+    } catch (err) {
+      console.log("Restore previous state err.");
+      throw err;
     }
   },
 };
